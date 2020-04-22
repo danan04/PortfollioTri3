64f58e7912e3b4493b2cbf836186dd6ee22a76ab
Catherine <45477636+CatherineGu16@users.noreply.github.com>
2020-04-21 23:15:56 -0700

    Made two new classes

---
+++ FourthClass.java
@@ -0,0 +1,12 @@
+
+	/*
+	 * Add new class at level of animal, cupcakes, alphabet
+	 * -build tester for new class to return array of data and verify loading into CircleQueue using CircleQueueDriver
+	 */
+
+public class FourthClass 
+{	
+	//
+	
+
+}
