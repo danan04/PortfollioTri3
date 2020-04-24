2f4f360e86ca8aff9655e5dce310c9c14fe858f7
Sravani <Sravani@LAPTOP-TR1PDCK6>
2020-04-23 23:55:01 -0700

    Finished CircleQueue AL Class. Not sure how good it is though

--- CircleQueueAL.java
+++ CircleQueueAL.java
@@ -5,6 +5,126 @@
  *
  *	Rewrite CircleQueue using arraylists
  */
-public class CircleQueueAL {
+import java.util.EmptyStackException;
+import java.util.Iterator;
 
+public class CircleQueueAL<T>
+{
+   private final int DEFAULT_CAPACITY = 100;
+   private int front, rear, count;
+   private T[] queue; 
+
+
+   //  Creates an empty queue using the default capacity.
+   public void CircularArrayQueue()
+   {
+      front = rear = count = 0;
+      queue = (T[]) (new Object[DEFAULT_CAPACITY]);
+   }
+
+
+   //  Creates an empty queue using the specified capacity.
+   public void CircularArrayQueue (int initialCapacity)
+   {
+      front = rear = count = 0;
+      queue = ( (T[])(new Object[initialCapacity]) );
+   }
+
+   //  Adds the specified element to the rear of the queue, expanding
+   //  the capacity of the queue array if necessary.
+   public void enqueue (T element)
+   {
+      if (size() == queue.length) 
+         expandCapacity();
+
+      queue[rear] = element;
+
+      rear = (rear+1) % queue.length;
+
+      count++;
+   }
+
+
+   //  Removes the element at the front of the queue and returns a
+   //  reference to it. Throws an EmptyCollectionException if the
+   //  queue is empty.
+   public T dequeue() throws EmptyStackException
+   {
+      if (isEmpty())
+         throw new EmptyStackException ();
+
+      T result = queue[front];
+      queue[front] = null;
+
+      front = (front+1) % queue.length;
+
+      count--;
+
+      return result;
+   }
+   
+
+   //  Returns a reference to the element at the front of the queue.
+   //  The element is not removed from the queue.  Throws an
+   //  EmptyCollectionException if the queue is empty.  
+   public T first() throws EmptyStackException
+   {
+      if (isEmpty())
+         throw new EmptyStackException (); 
+
+      return queue[front];
+   }
+
+   //  Returns true if this queue is empty and false otherwise. 
+   public boolean isEmpty()
+   {
+      return (count == 0);
+   }
+ 
+
+   //  Returns the number of elements currently in this queue.
+   public int size()
+   {
+      return count;
+   }
+
+
+   //-----------------------------------------------------------------
+   //  Returns a string representation of this queue. 
+   //-----------------------------------------------------------------
+  public String toString()
+  {
+    String result = "";
+    int scan = 0;
+ 
+    while(scan < count)
+    {
+     if(queue[scan]!=null)
+     {
+       result += queue[scan].toString()+"\n";
+     }
+    scan++;
+    }
+
+    return result;
+
+  }
+
+
+   //  Creates a new array to store the contents of the queue with
+   //  twice the capacity of the old one.
+  public void expandCapacity()
+  {
+    T[] larger = (T[])(new Object[queue.length *2]);   
+
+    for(int scan=0; scan < count; scan++)
+    {
+      larger[scan] = queue[front];
+      front=(front+1) % queue.length;
+    }
+
+    front = 0;
+    rear = count;
+    queue = larger;
+  }
 }
